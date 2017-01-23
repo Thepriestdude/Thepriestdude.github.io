7f0256eb8caee9eca8b86673d3838515b7b212be
OskarSelberg <oskar.selberg@gmail.com>
2017-01-24 00:49:22 +0100

    more changes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {Col, Row} from 'react-bootstrap'
 
 import './components.css'
+import cv from '../media/cv.pdf'
 
 export default class Body extends Component {
 
@@ -14,30 +15,29 @@ export default class Body extends Component {
               I'm the guy with a burning passion for software development, and
               to develop the future. Beyond the software projects I had during
               my education I have constantly been looking for new challenges to
-              develop myself both personally and intellectually. I love to
+              develop myself, both personally and intellectually. I love to
               solve problems with a group, and I'm usually the guy that comes
               with all the awesome (sometimes crazy) ideas how to solve them,
               while at the same time having an amazing eye for details, all to
               finally create a product that everyone can be proud of.
             </div>
           </Col>
-
           <Col xs={12} sm={6} md={6} lg={6}>
             <div className="info">
-              <a href="https://github.com/Thepriestdude" className="black">
-                <i className="fa fa-github" aria-hidden="true"/>&nbsp;&nbsp;
+              <a href="https://github.com/Thepriestdude" className="black" target="_blank">
+                <i className="fa fa-github" aria-hidden="true"/> &nbsp; &nbsp;
               </a>
-              <a href="https://www.linkedin.com/in/oskarselberg" className="black">
+              <a href="https://www.linkedin.com/in/oskarselberg" className="black" target="_blank">
                 <i className="fa fa-linkedin-square" aria-hidden="true"/>
-              </a>&nbsp;&nbsp;&nbsp;Follow me
+              </a> &nbsp;&nbsp;&nbsp;Follow me
             </div>
           </Col>
 
           <Col xs={12} sm={6} md={6} lg={6}>
             <div className="info">
-              <a href="../media/CV_Oskar_Selberg_Eng.pdf" className="black" download>
+              <a href={cv} className="black" download="Oskar_Selberg.pdf" target="_blank">
                 <i className="fa fa-file-text-o bigger" aria-hidden="true"/>
-              </a>&nbsp;&nbsp;Download my CV
+              </a> &nbsp; &nbsp;Download my CV
             </div>
           </Col>
         </Row>
